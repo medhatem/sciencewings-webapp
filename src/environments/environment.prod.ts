56d3e956db99397a83e6e160b80c8fa0b329a72b
Mostapha Abbad <gs.mostaphaabbad@gmail.com>
2022-02-12 07:46:17 -0500

    replace-keycloack-url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   // apiUrl: To replace when api is deployed in prod
-  keycloakUrl: 'https://sciencewings-keycloak.herokuapp.com',
+  keycloakUrl: 'http://keycloak-app-staging-env.eba-fsrexfym.ca-central-1.elasticbeanstalk.com',
   apiUrl: 'https://sciencewings-api-staging.herokuapp.com',
   swaggerUrl: 'https://sciencewings-api-staging.herokuapp.com/swagger/swagger.json',
   sciencewingsWebRealm: 'sciencewings-web',
