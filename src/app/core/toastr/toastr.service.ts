a9132905b3e1b670647d7b2eb7644fa330bd15a2
Borhaneddine Haddar <borhane.r.haddar@gamil.com>
2022-02-17 15:08:11 +0100

    removing unused UserProfileModule, correcting ToastrService Translation issue

--- src/app/core/toastr/toastr.service.ts
+++ src/app/core/toastr/toastr.service.ts
@@ -1,29 +1,41 @@
 import { Injectable } from '@angular/core';
 import { ToastrService as Toastr } from 'ngx-toastr';
+import { TranslocoService } from '@ngneat/transloco';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ToastrService {
-  constructor(private toastr: Toastr) {}
+  constructor(
+    private toastr: Toastr,
+    private _translocoService: TranslocoService,
+  ) {}
 
   showSuccess(message: string, title: string) {
-    const errorMessage = title; // TODO: Check for translation issues
-    this.toastr.success(message, errorMessage);
+    this.toastr.success(
+      this._translocoService.translate(message),
+      this._translocoService.translate(title)
+    );
   }
 
   showError(message: string, title: string) {
-    const errorMessage = title; // TODO: Check for translation issues
-    this.toastr.error(message, errorMessage);
+    this.toastr.error(
+      this._translocoService.translate(message),
+      this._translocoService.translate(title)
+    );
   }
 
   showInfo(message: string, title: string) {
-    const errorMessage = title; // TODO: Check for translation issues
-    this.toastr.info(message, errorMessage);
+    this.toastr.info(
+      this._translocoService.translate(message),
+      this._translocoService.translate(title)
+    );
   }
 
   showWarning(message: string, title: string) {
-    const errorMessage = title; // TODO: Check for translation issues
-    this.toastr.warning(message, errorMessage);
+    this.toastr.warning(
+      this._translocoService.translate(message),
+      this._translocoService.translate(title)
+    );
   }
 }
