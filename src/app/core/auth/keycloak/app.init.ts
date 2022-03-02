25a2b771f33f65ff8911a715c44f37564806f551
Mostapha Abbad <gs.mostaphaabbad@gmail.com>
2022-03-02 13:35:22 -0500

    add local and change keycloak link

--- src/app/core/auth/keycloak/app.init.ts
+++ src/app/core/auth/keycloak/app.init.ts
@@ -1,23 +1,21 @@
+/* eslint-disable no-console */
 import { KEYCLOAK_BEARER } from '@fuse/services/config/config.constants';
+import { environment } from 'environments/environment';
 import { KeycloakService } from 'keycloak-angular';
-import { EnvService } from '../../../environment/env.service';
 
-export const initializeKeycloak = (keycloak: KeycloakService) => {
-  const env = new EnvService();
-  return () =>
-    keycloak.init({
-      config: {
-        url: `${env.keycloakUrl}/auth`,
-        realm: env.sciencewingsWebRealm,
-        clientId: env.clientId,
-      },
-      initOptions: {
-        checkLoginIframe: true,
-        checkLoginIframeInterval: 25,
-      },
-      loadUserProfileAtStartUp: true,
-      enableBearerInterceptor: true,
-      bearerPrefix: KEYCLOAK_BEARER,
-      bearerExcludedUrls: ['/assets'],
-    });
-};
+export const initializeKeycloak = (keycloak: KeycloakService) => () =>
+  keycloak.init({
+    config: {
+      url: `${environment.keycloakUrl}/auth`,
+      realm: environment.sciencewingsWebRealm,
+      clientId: environment.clientId,
+    },
+    initOptions: {
+      checkLoginIframe: true,
+      checkLoginIframeInterval: 25,
+    },
+    loadUserProfileAtStartUp: true,
+    enableBearerInterceptor: true,
+    bearerPrefix: KEYCLOAK_BEARER,
+    bearerExcludedUrls: ['/assets'],
+  });
