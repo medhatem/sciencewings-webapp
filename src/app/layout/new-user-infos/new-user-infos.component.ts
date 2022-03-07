fb00172bd07fb5464b6a4ded85c5acebffe1265c
Borhaneddine Haddar <borhane.r.haddar@gamil.com>
2022-03-07 23:48:13 +0100

    New user stepper - step 2 - addresses formarray, work in progress

--- src/app/layout/new-user-infos/new-user-infos.component.ts
+++ src/app/layout/new-user-infos/new-user-infos.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 import { NewUserInfosResolver } from './new-user-infos.resolver';
 import { User, Phone, Address } from 'app/models';
+import { ToastrService } from 'app/core/toastr/toastr.service';
 
 @Component({
   selector: 'new-user-infos',
@@ -11,9 +13,19 @@ export class NewUserInfosComponent implements OnInit {
   @Input() hideMenusAndButtons: boolean;
   @Output() onFormComplete = new EventEmitter<boolean>();
   user: any;
+  countries: any;
   stepperForm: FormGroup;
 
-  constructor(private _newUserInfosResolver: NewUserInfosResolver, private _formBuilder: FormBuilder) {}
+  constructor(
+    private _newUserInfosResolver: NewUserInfosResolver,
+    private _formBuilder: FormBuilder,
+    private _toastr: ToastrService,
+    private _http: HttpClient,
+  ) {}
+
+  get addresses(): FormArray {
+    return (this.stepperForm.controls['step2'] as FormGroup).controls['addresses'] as FormArray;
+  }
 
   emitOnFormComplete() {
     // false to emit that : hideMenusAndButtons = false
@@ -21,7 +33,13 @@ export class NewUserInfosComponent implements OnInit {
   }
 
   async ngOnInit() {
-    this.user = await this._newUserInfosResolver.getloadUserProfileKeycloak();
+    this._prepareCountries();
+
+    try {
+      this.user = await this._newUserInfosResolver.getloadUserProfileKeycloak();
+    } catch (err) {
+      this._toastr.showError(err);
+    }
 
     this.stepperForm = this._formBuilder.group({
       step1: this._formBuilder.group({
@@ -31,20 +49,31 @@ export class NewUserInfosComponent implements OnInit {
         dateOfBirth: ['', Validators.required],
       }),
       step2: this._formBuilder.group({
-        apartNumber: [''],
-        about: [''],
-        streetNumber: [''],
-        street: [''],
-        country: [''],
-        province: [''],
-        postalCode: [''],
-        department: [''],
-        sector: [''],
-        facebook: [''],
-        linkedin: [''],
-        twitter: [''],
-        other: [''],
+        phones: this._formBuilder.array([]),
+        addresses: this._formBuilder.array([]),
       }),
     });
+
+    (this.stepperForm.controls['step2'] as FormGroup).controls['addresses'] = this._formBuilder.group({
+      id: 0,
+      street: '',
+      apartment: '',
+      province: '',
+      city: '',
+      code: '',
+      country: 'Algeria',
+      type: 'USER',
+    });
+  }
+
+  getCountryByName(name: string) {
+    if (this.countries) {
+      return this.countries.find((country) => country.name === name);
+    }
+  }
+
+  private _prepareCountries() {
+    // TODO: retrieve countries from the backend instead of mock api
+    this._http.get('api/apps/contacts/countries').subscribe((countries) => (this.countries = countries));
   }
 }
