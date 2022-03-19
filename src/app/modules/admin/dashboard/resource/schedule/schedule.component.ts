554ab030d2888966c8b3917acedd1cd8e5e1e56b
splimter <merah.soheyb@gmail.com>
2022-03-19 23:35:26 +0100

    draft

--- src/app/modules/admin/dashboard/resource/schedule/schedule.component.ts
+++ src/app/modules/admin/dashboard/resource/schedule/schedule.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, Renderer2, OnInit, ViewChild } from '@angular/core';
 import { CalendarOptions, FullCalendarComponent } from '@fullcalendar/angular';
+import { EventMountArg } from '@fullcalendar/core';
 import listPlugin from '@fullcalendar/list';
 import { ResourceService } from 'app/modules/admin/resolvers/resource/resource.service';
 import { ToastrService } from 'app/core/toastr/toastr.service';
@@ -14,36 +15,78 @@ export class ResourceScheduleComponent implements OnInit {
   calendarOptions: CalendarOptions = {
     plugins: [listPlugin],
     initialView: 'listWeek',
+    themeSystem: 'litera',
+    selectable: true,
+    headerToolbar: {
+      left: 'dayGridMonth,timeGridWeek,timeGridDay custom1',
+      right: 'custom2 prevYear,prev,next,nextYear',
+    },
+    eventDidMount: (mountAg: EventMountArg) => {
+      console.log({ mountAg });
+
+      const checkbox = this.renderer.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.setAttribute('id', mountAg.event._def.publicId);
+      checkbox.setAttribute('style', 'margin-top: 14px;');
+      mountAg.el.appendChild(checkbox);
+      this.renderer.listen(checkbox, 'change', ({ target }) => {
+        if (target.checked) {
+          this.selectedEvents.push(target.id);
+        } else {
+          this.selectedEvents.splice(this.selectedEvents.indexOf(target.id), 1);
+        }
+        console.log(this.selectedEvents);
+      });
+    },
     events: [
       {
-        title: 'Meeting',
-        start: new Date(),
+        title: 'PAST',
+        start: new Date('2022-03-18T14:00:00.000+01:00'),
+        end: new Date('2022-03-18T20:00:00.000+01:00'),
+        id: '1',
         extendedProps: {
           status: 'done',
+          eventID: 1,
         },
       },
       {
-        title: 'Birthday Party',
-        start: new Date(),
+        title: 'TODAY - MORNING',
+        start: new Date('2022-03-19T07:00:00.000+01:00'),
+        end: new Date('2022-03-19T07:12:00.000+01:00'),
         backgroundColor: 'green',
         borderColor: 'green',
+        id: '2',
       },
       {
-        title: 'Birthday Party II',
-        start: new Date(),
+        title: 'TODAY - NOW',
+        start: new Date('2022-03-19T19:00:00.000+01:00'),
+        end: new Date('2022-03-19T21:00:00.000+01:00'),
+        backgroundColor: 'green',
+        borderColor: 'green',
+        id: '2',
+      },
+      {
+        title: 'TODAY - LATE',
+        start: new Date('2022-03-19T22:00:00.000+01:00'),
+        end: new Date('2022-03-19T23:00:00.000+01:00'),
+        backgroundColor: 'green',
+        borderColor: 'green',
+        id: '2',
+      },
+      {
+        title: 'FUTURE',
+        start: new Date('2022-03-20T07:00:00.000+01:00'),
+        end: new Date('2022-03-20T11:00:00.000+01:00'),
         backgroundColor: 'red',
         borderColor: 'green',
+        id: '3',
       },
     ],
   };
 
   resources = [];
-
-  constructor(
-    private _resourceService: ResourceService,
-    private _toastrService: ToastrService,
-    private _changeDetectorRef: ChangeDetectorRef,
-  ) {}
+  private selectedEvents = [];
+  constructor(private _resourceService: ResourceService, private _toastrService: ToastrService, private _changeDetectorRef: ChangeDetectorRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
     this._resourceService.getOrgResource().subscribe(({ statusCode, body, errorMessage }) => {
@@ -67,5 +110,9 @@ export class ResourceScheduleComponent implements OnInit {
       this.calendarComponent.getApi().addEvent(events);
       this.calendarOptions.events = events;
     });
+  }
+
+  onCheckboxChange($event) {
+    console.log($event);
   }
 }
