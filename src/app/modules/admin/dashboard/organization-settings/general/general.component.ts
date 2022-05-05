31b939e205d144ed818ecf480594f50b96bf180d
splimter <merah.soheyb@gmail.com>
2022-05-05 15:28:34 +0100

    base ui

--- src/app/modules/admin/dashboard/organization-settings/general/general.component.ts
+++ src/app/modules/admin/dashboard/organization-settings/general/general.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ToastrService } from 'app/core/toastr/toastr.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'organization-settings-general',
   templateUrl: './general.component.html',
-  styleUrls: ['./general.component.scss']
+  styleUrls: ['./general.component.scss'],
 })
 export class GeneralComponent implements OnInit {
+  form: FormGroup;
+  constructor(private _formBuilder: FormBuilder, private _toastrService: ToastrService, private _coookies: CookieService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  onSubmit() {}
 }
