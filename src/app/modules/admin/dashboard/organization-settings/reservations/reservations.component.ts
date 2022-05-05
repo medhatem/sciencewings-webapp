dd5b08482bfb2481d71aba17292c4e6f5735aa72
splimter <merah.soheyb@gmail.com>
2022-05-05 23:33:11 +0100

    finilizing ui

--- src/app/modules/admin/dashboard/organization-settings/reservations/reservations.component.ts
+++ src/app/modules/admin/dashboard/organization-settings/reservations/reservations.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ToastrService } from 'app/core/toastr/toastr.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'organization-settings-reservations',
   templateUrl: './reservations.component.html',
-  styleUrls: ['./reservations.component.scss']
+  styleUrls: ['./reservations.component.scss'],
 })
 export class ReservationsComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  constructor(private _formBuilder: FormBuilder, private _toastrService: ToastrService, private _coookies: CookieService) {}
 
   ngOnInit(): void {
+    this.form = this._formBuilder.group({
+        x: true, y: 1, z: ''
+    });
   }
 
+  onSubmit() {}
 }
