3c182c8f646599f57ed45b2724219e74ff5a7ea7
splimter <merah.soheyb@gmail.com>
2022-05-10 13:48:56 +0100

    finishing figma based desing

--- src/app/modules/admin/dashboard/organization-settings/members/members.component.ts
+++ src/app/modules/admin/dashboard/organization-settings/members/members.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ToastrService } from 'app/core/toastr/toastr.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'organization-settings-members',
   templateUrl: './members.component.html',
-  styleUrls: ['./members.component.scss']
+  styleUrls: ['./members.component.scss'],
 })
 export class MembersComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder, private _toastrService: ToastrService, private _coookies: CookieService) {}
 
   ngOnInit(): void {
+    this.form = this._formBuilder.group({
+      x: true,
+      y: 1,
+      z: '',
+    });
   }
 
+  onSubmit() {}
 }
