1e1bd9a37ed9bdef227961e881ca03fa870f7c65
splimter <merah.soheyb@gmail.com>
2022-05-12 17:43:38 +0100

    adding update service

--- src/app/modules/admin/dashboard/organization-settings/members/members.component.ts
+++ src/app/modules/admin/dashboard/organization-settings/members/members.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ToastrService } from 'app/core/toastr/toastr.service';
-import { CookieService } from 'ngx-cookie-service';
+import { AdminOrganizationsService } from 'app/modules/admin/resolvers/admin-organization/admin-organization.service';
 
 @Component({
   selector: 'organization-settings-members',
@@ -9,17 +9,42 @@ import { CookieService } from 'ngx-cookie-service';
   styleUrls: ['./members.component.scss'],
 })
 export class MembersComponent implements OnInit {
+  @Input() settings: any;
+  @Output() updateLocalSettings = new EventEmitter<string>();
   form: FormGroup;
+  isAccountNumberNote = false;
 
-  constructor(private _formBuilder: FormBuilder, private _toastrService: ToastrService, private _coookies: CookieService) {}
+  constructor(private _formBuilder: FormBuilder, private _toastrService: ToastrService, private organizationService: AdminOrganizationsService) {}
 
   ngOnInit(): void {
     this.form = this._formBuilder.group({
-      x: true,
-      y: 1,
-      z: '',
+      membersCanEditAccountNumbers: true,
+      promptForAccouantNumbers: true,
+      acountNumberNote: '',
+      allowMembersToSeeAllOtherMembers: true,
+    });
+    this.isAccountNumberNote = !!this.settings.acountNumberNote;
+    this.form.setValue({
+      membersCanEditAccountNumbers: this.settings.membersCanEditAccountNumbers,
+      promptForAccouantNumbers: this.settings.promptForAccouantNumbers,
+      acountNumberNote: this.settings.acountNumberNote,
+      allowMembersToSeeAllOtherMembers: this.settings.allowMembersToSeeAllOtherMembers,
     });
   }
+  accountNumberNoteListener(event) {
+    this.isAccountNumberNote = event.checked;
+  }
 
-  onSubmit() {}
+  onSubmit() {
+    const data = { ...this.form.value };
+
+    this.organizationService.updateOrganizationsSettingsProperties(1, data).subscribe((response) => {
+      if (response.body.statusCode === 204) {
+        this.updateLocalSettings.emit(this.form.value);
+        this._toastrService.showSuccess('Updated Successfully');
+      } else {
+        this._toastrService.showError('Something went wrong!');
+      }
+    });
+  }
 }
