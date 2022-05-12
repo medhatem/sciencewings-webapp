87f72cb752599e2db692c48b3748b9678e5639fa
splimter <merah.soheyb@gmail.com>
2022-05-12 15:30:26 +0100

    adding update service

--- src/app/modules/admin/dashboard/organization-settings/access/access.component.ts
+++ src/app/modules/admin/dashboard/organization-settings/access/access.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ToastrService } from 'app/core/toastr/toastr.service';
+import { AdminOrganizationsService } from 'app/modules/admin/resolvers/admin-organization/admin-organization.service';
 import { CookieService } from 'ngx-cookie-service';
 
 @Component({
@@ -9,17 +10,49 @@ import { CookieService } from 'ngx-cookie-service';
   styleUrls: ['./access.component.scss'],
 })
 export class AccessComponent implements OnInit {
+  @Input() settings: any;
+  @Output() updateLocalSettings = new EventEmitter<string>();
   form: FormGroup;
-
-  constructor(private _formBuilder: FormBuilder, private _toastrService: ToastrService, private _coookies: CookieService) {}
+  isMemberShouldAccessByJoinCode = false;
+  constructor(private _formBuilder: FormBuilder, private _toastrService: ToastrService, private organizationService: AdminOrganizationsService) {}
 
   ngOnInit(): void {
     this.form = this._formBuilder.group({
-      x: true,
-      y: 1,
-      z: '',
+      anyMemberCanJoinYourOrganizationAndAccessResourceSchedules: true,
+      joinCode: '',
+      yourOrganizationWillNeverAppearInSearchResults: true,
+      notifyAdministratorsWhenMembersJoinOrganization: true,
+      listResourceToNonMembers: true,
+      messageSentToNewMembers: '',
+    });
+    this.isMemberShouldAccessByJoinCode = !!this.settings.joinCode;
+    console.log({isMemberShouldAccessByJoinCode: this.isMemberShouldAccessByJoinCode});
+
+    this.form.setValue({
+      anyMemberCanJoinYourOrganizationAndAccessResourceSchedules: this.settings.anyMemberCanJoinYourOrganizationAndAccessResourceSchedules,
+      joinCode: this.settings.joinCode,
+      yourOrganizationWillNeverAppearInSearchResults: this.settings.yourOrganizationWillNeverAppearInSearchResults,
+      notifyAdministratorsWhenMembersJoinOrganization: this.settings.notifyAdministratorsWhenMembersJoinOrganization,
+      listResourceToNonMembers: this.settings.listResourceToNonMembers,
+      messageSentToNewMembers: this.settings.messageSentToNewMembers,
     });
   }
 
-  onSubmit() {}
+  onSubmit() {
+    const data = { ...this.form.value };
+    data.joinCode = this.isMemberShouldAccessByJoinCode ? data.joinCode : null;
+
+    this.organizationService.updateOrganizationsSettingsProperties(1, data).subscribe((response) => {
+      if (response.body.statusCode === 204) {
+        this.updateLocalSettings.emit(this.form.value);
+        this._toastrService.showSuccess('Updated Successfully');
+      } else {
+        this._toastrService.showError('Something went wrong!');
+      }
+    });
+  }
+
+  memberShouldAccessByJoinCodeListener(event) {
+    this.isMemberShouldAccessByJoinCode = event.checked;
+  }
 }
