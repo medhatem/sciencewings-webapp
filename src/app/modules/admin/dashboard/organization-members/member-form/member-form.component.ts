9893c3ebc741623824308101c10e5ff6c2fb8d08
splimter <merah.soheyb@gmail.com>
2022-06-02 13:47:05 +0100

    chkpnt

--- src/app/modules/admin/dashboard/organization-members/member-form/member-form.component.ts
+++ src/app/modules/admin/dashboard/organization-members/member-form/member-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { ToastrService } from 'app/core/toastr/toastr.service';
+import { MemberService } from 'app/modules/admin/resolvers/members/member.service';
 
 @Component({
   selector: 'member-form',
@@ -11,12 +13,24 @@ export class MemberFormComponent implements OnInit {
   memberForm: FormGroup;
   isInvitationPersonalize: boolean = false;
 
-  constructor(public matDialogRef: MatDialogRef<MemberFormComponent>, private _formBuilder: FormBuilder) {}
+  constructor(
+    public matDialogRef: MatDialogRef<MemberFormComponent>,
+    private _toastrService: ToastrService,
+    private _formBuilder: FormBuilder,
+    private _memberService: MemberService,
+  ) {}
 
   ngOnInit(): void {
-    // Create the form
     this.memberForm = this._formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  public invite(): void {
+    this._memberService.inviteUserToOrganization(1, this.memberForm.value.email).subscribe(({ body }) => {
+      if (body.statusCode === 500) {
+        this._toastrService.showError('Something went wrong!');
+      }
     });
   }
 }
