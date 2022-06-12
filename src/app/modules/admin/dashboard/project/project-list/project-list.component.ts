ba2691849da7fabcba4f1d00444a44e1d564d3e0
bassetsadok <salah.sadok67@gmail.com>
2022-06-13 00:18:46 +0100

    create the project table with necessary buttons without services

--- src/app/modules/admin/dashboard/project/project-list/project-list.component.ts
+++ src/app/modules/admin/dashboard/project/project-list/project-list.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-project-list',
   templateUrl: './project-list.component.html',
-  styleUrls: ['./project-list.component.scss']
+  styleUrls: ['./project-list.component.scss'],
 })
 export class ProjectListComponent implements OnInit {
+  resources = [
+    {
+      name: 'string1',
+      description: 'string1',
+      resourceType: 'string1',
+      resourceClass: 'string1',
+      timezone: 'string1  ',
+      organization: 1,
+      user: 1,
+    },
+    {
+      name: 'string2',
+      description: 'string2',
+      resourceType: 'string2',
+      resourceClass: 'string2',
+      timezone: 'string2',
+      organization: 2,
+      user: 2,
+    },
+  ];
 
-  constructor() { }
+  searchInputControl: FormControl = new FormControl();
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
--- src/app/modules/admin/dashboard/project/project.module.ts
+++ src/app/modules/admin/dashboard/project/project.module.ts
@@ -5,10 +5,12 @@ import { RouterModule } from '@angular/router';
 import { projectRoutes } from './project.routing';
 import { FuseFindByKeyPipeModule } from '@fuse/pipes/find-by-key/find-by-key.module';
 import { SharedModule } from 'app/shared/shared.module';
-import { NgSelectModule } from '@ng-select/ng-select/lib/ng-select.module';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { TranslocoModule } from '@ngneat/transloco';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [ProjectComponent, ProjectListComponent],
-  imports: [RouterModule.forChild(projectRoutes), FuseFindByKeyPipeModule, SharedModule],
+  imports: [RouterModule.forChild(projectRoutes), FuseFindByKeyPipeModule, SharedModule, NgSelectModule, TranslocoModule, MatFormFieldModule],
 })
 export class ProjectModule {}
