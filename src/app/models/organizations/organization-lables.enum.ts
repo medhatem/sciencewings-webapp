6d38cbdfc325bdd4b2fa8f091467a89713cab8b5
abdesselam <xpack94@outlook.com>
2022-06-26 04:02:56 -0400

    update organization creation form

---
+++ src/app/models/organizations/organization-lables.enum.ts
@@ -0,0 +1,8 @@
+export enum OrganizationLabelsTranslation {
+  personal = 'ORGANIZATION.FORM.STEP1.PERSONAL',
+  private = 'ORGANIZATION.FORM.STEP1.PRIVATE',
+}
+export enum OrganizationLabels {
+  personal = 'Personal',
+  private = 'Private',
+}
