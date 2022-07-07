6740123fbdf1b0db99574136b3d92f11dc40d45f
bassetsadok <salah.sadok67@gmail.com>
2022-07-07 11:18:14 +0100

    update the form of create projects

--- src/app/modules/admin/dashboard/project/project-form/project-form.component.ts
+++ src/app/modules/admin/dashboard/project/project-form/project-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { ProjectType, ProjectTypeTrasnlation } from 'app/models/projects/project-type';
 
 @Component({
   selector: 'app-project-form',
@@ -10,6 +11,9 @@ import { MatDialogRef } from '@angular/material/dialog';
 export class ProjectFormComponent implements OnInit {
   projectForm: FormGroup;
   isInvitationPersonalize: boolean = false;
+  projectTypesKeys = Object.keys(ProjectType).map((key) => key);
+  projectType = ProjectType;
+  projectTypeTrasnlation = ProjectTypeTrasnlation;
 
   constructor(public matDialogRef: MatDialogRef<ProjectFormComponent>, private _formBuilder: FormBuilder) {}
 
@@ -17,7 +21,20 @@ export class ProjectFormComponent implements OnInit {
     this.projectForm = this._formBuilder.group({
       name: ['', [Validators.required, Validators.name]],
       member: [''],
+      number: [''],
+      location: [''],
       description: [''],
+      type: [''],
     });
+  }
+  /**
+   *
+   * Used to track for loops elements by either their id or index
+   *
+   * @param index index of the element to track
+   * @param item to track
+   */
+  trackByFn(index: number, item: any): any {
+    return item.id || index;
   }
 }
