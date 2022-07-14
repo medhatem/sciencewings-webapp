886d4028dce1247c215cb8250893b736dc104051
abdesselam <xpack94@outlook.com>
2022-07-14 03:06:05 -0400

    fix build

--- src/app/modules/admin/dashboard/organization-profile/admin-organization.component.ts
+++ src/app/modules/admin/dashboard/organization-profile/admin-organization.component.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { Router } from '@angular/router';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
 import { constants } from 'app/shared/constants';
 
 @Component({
@@ -8,13 +8,6 @@ import { constants } from 'app/shared/constants';
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AdminOrganizationComponent implements OnInit {
+export class AdminOrganizationComponent {
   readonly organizationProfilePath = `/${constants.MODULES_ROUTINGS_URLS.ADMIN}/${constants.MODULES_ROUTINGS_CHILDREN_URLS.ADMIN.ORGANIZATION_PROFILE}`;
-
-  constructor(private _router: Router) {}
-
-  ngOnInit() {
-    const currentOrgId = Number(localStorage.getItem(constants.CURRENT_ORGANIZATION_ID)) || '';
-    this._router.navigate([this.organizationProfilePath, currentOrgId]);
-  }
 }
