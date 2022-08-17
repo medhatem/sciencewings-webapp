0c29ad15d9a9d0ce7e5936fffc54b0a734bf40ba
bassetsadok <salah.sadok67@gmail.com>
2022-08-17 03:11:40 +0100

    take off project settings

--- src/app/modules/admin/dashboard/project/project-settings/project-settings.routing.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '@angular/router';
-import { ProjectSettingsComponent } from './project-settings.component';
-
-export const projectSettingsRoutes: Route[] = [
-  {
-    path: '',
-    component: ProjectSettingsComponent,
-  },
-];
