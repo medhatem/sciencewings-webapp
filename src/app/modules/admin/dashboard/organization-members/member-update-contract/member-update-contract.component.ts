25a171db01df9f2895e6476e89a77d9700b3d06a
bassetsadok <salah.sadok67@gmail.com>
2022-09-10 20:22:28 +0100

    update job name

--- src/app/modules/admin/dashboard/organization-members/member-update-contract/member-update-contract.component.ts
+++ src/app/modules/admin/dashboard/organization-members/member-update-contract/member-update-contract.component.ts
@@ -6,6 +6,8 @@ import { ContractRo, ContractType, GetContract, JobLevel } from 'app/models/cont
 import { ContractService } from 'app/modules/admin/resolvers/contract/contract.service';
 import { constants } from 'app/shared/constants';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MemberService } from 'app/modules/admin/resolvers/members/member.service';
+import { OrganizationMembers } from 'app/models/members/member';
 
 @Component({
   selector: 'app-member-update-contract',
@@ -17,24 +19,30 @@ export class MemberUpdateContractComponent implements OnInit {
   jobLevels = JobLevel;
   contractForm: FormGroup;
   contracts: any[] = [];
+  supervisors: any;
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: { orgId: number; userId: number; contractDto: any },
     private _route: ActivatedRoute,
     private _formBuilder: FormBuilder,
     private _contractService: ContractService,
+    private _memberService: MemberService,
     private _toastrService: ToastrService,
     private _router: Router,
     private _cdr: ChangeDetectorRef,
   ) {}
 
   ngOnInit(): void {
+    this._memberService.getOrgMembers().subscribe(({ body }) => {
+      this.supervisors = body.data.map((member) => new OrganizationMembers(member));
+    });
     const contractFormObj = {
-      jobName: [this.data?.contractDto?.job?.name || '', [Validators.required]],
+      name: [this.data?.contractDto?.job?.name || '', [Validators.required]],
       jobLevel: [this.data?.contractDto?.jobLevel || '', [Validators.required]],
       contractType: [this.data?.contractDto?.contractType || '', [Validators.required]],
       dateStart: [this.data?.contractDto?.dateStart || '', [Validators.required]],
       dateEnd: [this.data?.contractDto?.endDate || ''],
       description: [this.data?.contractDto?.description || '', [Validators.required]],
+      supervisor: [this.data?.contractDto?.supervisor, [Validators.required]],
     };
     this.contractForm = this._formBuilder.group(contractFormObj);
   }
@@ -79,5 +87,14 @@ export class MemberUpdateContractComponent implements OnInit {
   }
   private getOrganizationIdFromLocalStorage(): number {
     return Number(localStorage.getItem(constants.CURRENT_ORGANIZATION_ID));
+  }
+  /**
+   * Track by function for ngFor loops
+   *
+   * @param index
+   * @param item
+   */
+  trackByFn(index: number, item: any): any {
+    return item.id || index;
   }
 }
