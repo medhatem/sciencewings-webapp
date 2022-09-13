7ab49e66dde0d840c834b4e63cb700965c575751
bassetsadok <salah.sadok67@gmail.com>
2022-09-13 21:52:13 +0100

    fix

--- src/app/modules/admin/dashboard/project/project-settings/project-settings.module.ts
+++ src/app/modules/admin/dashboard/project/project-settings/project-settings.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectSettingsComponent } from './project-settings.component';
+import { projectSettingsRoutes } from './project-settings.routing';
 import { RouterModule } from '@angular/router';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { SharedModule } from 'app/shared/shared.module';
