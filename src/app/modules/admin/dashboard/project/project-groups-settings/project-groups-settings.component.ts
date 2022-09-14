10351a9285c2ee2d5750833b61112138fbd609bd
bassetsadok <salah.sadok67@gmail.com>
2022-09-14 04:39:42 +0100

    group settings

--- src/app/modules/admin/dashboard/project/project-groups-settings/project-groups-settings.component.ts
+++ src/app/modules/admin/dashboard/project/project-groups-settings/project-groups-settings.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-project-groups-settings',
   templateUrl: './project-groups-settings.component.html',
-  styleUrls: ['./project-groups-settings.component.scss']
+  styleUrls: ['./project-groups-settings.component.scss'],
 })
 export class ProjectGroupsSettingsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
