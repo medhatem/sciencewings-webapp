2cffb57f067793cb4caf2b0b333ad41f30235f81
Mdiabi <mdiabi@sciencewings.com>
2022-11-02 20:27:32 +0000

    Revert "Revert "Feature/WEB-279 fix list of groups not refreshing after submiting the create form (pull request #173)" (pull request #181)"

--- src/app/modules/admin/dashboard/project/project.routing.ts
+++ src/app/modules/admin/dashboard/project/project.routing.ts
@@ -4,6 +4,7 @@ import { ProjectComponent } from './project.component';
 import { AdminOrganizationResolver } from '../../resolvers/admin-organization/admin-organization.resolvers';
 import { ProjectSettingsComponent } from './project-settings/project-settings.component';
 import { AddMemberToProjectComponent } from './add-member-to-project/add-member-to-project.component';
+import { ProjectResolver } from '../../resolvers/project/project.resolvers';
 
 export const projectRoutes: Route[] = [
   {
@@ -13,6 +14,9 @@ export const projectRoutes: Route[] = [
       {
         path: '',
         component: ProjectListComponent,
+        resolve: {
+          project: ProjectResolver,
+        },
       },
       {
         path: 'project-settings/:id',
