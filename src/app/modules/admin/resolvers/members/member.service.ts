70098eee4b5fbc291073d52932761e3328406512
bassetsadok <salah.sadok67@gmail.com>
2022-11-11 17:01:50 +0100

    member can have more than one role

--- src/app/modules/admin/resolvers/members/member.service.ts
+++ src/app/modules/admin/resolvers/members/member.service.ts
@@ -115,7 +115,7 @@ export class MemberService {
     );
   }
 
-  inviteUserToOrganization(organizationId: number, email: string, role: string): Observable<any> {
+  inviteUserToOrganization(organizationId: number, email: string, role: string[]): Observable<any> {
     return this.swaggerAPI.memberRoutesInviteUserToOrganization({ body: { organizationId, email, role } });
   }
 
